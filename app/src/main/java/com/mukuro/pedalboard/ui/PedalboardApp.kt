2090ae02f04072cecae59044c1c79d75a5c3a93a
Arutemu <exoticelf2009@gmail.com>
2024-04-02 02:46:45 +0300

    Experimental: - finally have behavior similar to the desired one. --- however, there are a lot of issues to fix in order to make it working fully --- need to remove the previous NavRail --- will probably broke the gesture to open the drawer (would like to leave this feature)

--- app/src/main/java/com/mukuro/pedalboard/ui/PedalboardApp.kt
+++ app/src/main/java/com/mukuro/pedalboard/ui/PedalboardApp.kt
@@ -4,9 +4,11 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.expandHorizontally
 import androidx.compose.animation.shrinkHorizontally
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -24,6 +26,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.AbsoluteAlignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -130,16 +133,32 @@ fun PedalboardApp(
         PedalboardTopBar(onDrawerClicked = {
 
         })
-        PedalboardNavigationWrapper(
-            navigationType = navigationType,
-            contentType = contentType,
-            displayFeatures = displayFeatures,
-            navigationContentPosition = navigationContentPosition,
-            pedalboardHomeUIState = pedalboardHomeUIState,
-            closeDetailScreen = closeDetailScreen,
-            navigateToDetail = navigateToDetail,
-            toggleSelectedPlugin = toggleSelectedPlugin
-        )
+        Box(modifier = Modifier) {
+
+
+
+            Row(modifier = Modifier){
+                Column(modifier = Modifier.width(80.dp)){ }
+                PedalboardNavigationWrapper(
+                    navigationType = navigationType,
+                    contentType = contentType,
+                    displayFeatures = displayFeatures,
+                    navigationContentPosition = navigationContentPosition,
+                    pedalboardHomeUIState = pedalboardHomeUIState,
+                    closeDetailScreen = closeDetailScreen,
+                    navigateToDetail = navigateToDetail,
+                    toggleSelectedPlugin = toggleSelectedPlugin
+                )
+            }
+
+            PedalboardNavigationRail(
+                selectedDestination = "Quick",
+                navigationContentPosition = PedalboardNavigationContentPosition.CENTER ,
+                navigateToTopLevelDestination = {}
+            )
+
+        }
+
     }
 
 }
